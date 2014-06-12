c173e277870d25bc6c8046d0ba329d70ef14680e
Devin Dreszer <devindreszer@gmail.com>
2014-06-12 17:51:36 -0400

    Refactor code to have separate depreciation methods
    * Method for each depreciation
    * Create depreciation value method to reduce from original msrp

--- lib/used_car.rb
+++ lib/used_car.rb
@@ -7,10 +7,27 @@ class UsedCar < Car
 
   def initialize(make, model, year, msrp)
     super
+    @damages = {}
+  end
+
+  def add_damage(damage)
+    @damages[damage.description] = damage.cost
+  end
+
+  def mileage_depreciation
+    @msrp * (mileage * @@mileage_depreciation_rate)
+  end
+
+  def damage_depreciation
+    damage_cost = 0
+    @damages.each do |damage, cost|
+      damage_cost += cost
+    end
+    damage_cost
   end
 
   def depreciated_value
-    age_depreciated_value * (1 - mileage * @@mileage_depreciation_rate)
+    [(@msrp - age_depreciation - mileage_depreciation - damage_depreciation), 0].max
   end
 
   def price
